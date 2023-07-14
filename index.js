325fcd6b5bc8e2b87fc2dbc4b11e211cd5f12945
Chetan Singh <chetansingh@Chetans-MacBook-Pro.local>
2023-07-14 17:05:47 +0530

    day 13

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const bodyParser = require('body-parser');
 require('./db')
 require('./models/User')
 const authRoutes= require('./routes/authRoutes');
+const uploadMediaRoutes=require('./routes/uploadMediaRoutes')
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(uploadMediaRoutes)
 app.get('/',(req,res)=>{
     res.send("Helloe World");
 })
